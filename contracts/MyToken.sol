b00fd7872daf5b7a3296c72e289e94881f540a3d
onebronze467 <onebronze@naver.com>
2026-04-14 21:52:40 +0900

    Refactoring Test

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -10,12 +10,12 @@ contract MyToken {
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
 
-    constructor(string memory _name, string memory _symbol, uint8 _decimals) {
+    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _amount) {
         name = _name;
         symbol = _symbol;
         decimals = _decimals;
 
-        _mint(1 * 10 ** uint256(decimals), msg.sender); // 1 MT
+        _mint(_amount * 10 ** uint256(decimals), msg.sender); // 1 MT
     }
 
     function _mint(uint256 amount, address owner) internal {
