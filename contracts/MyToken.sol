b7c9b334bd23162448924d2f1f8073cbe41e55a2
onebronze467 <onebronze@naver.com>
2026-04-14 21:31:28 +0900

    mint function

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -14,6 +14,13 @@ contract MyToken {
         name = _name;
         symbol = _symbol;
         decimals = _decimals;
+
+        _mint(1 * 10 ** uint256(decimals), msg.sender); // 1 MT
+    }
+
+    function _mint(uint256 amount, address owner) internal {
+        totalSupply += amount;
+        balanceOf[owner] += amount;
     }
 
     // function totalSupply() external view  returns (uint256) {
