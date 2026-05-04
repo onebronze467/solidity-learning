3373c955b0dd4c3157bb7607bacf4bcb80e7d6c4
onebronze467 <onebronze@naver.com>
2026-05-04 19:33:05 +0900

    add mint

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -36,6 +36,10 @@ contract MyToken {
         
     }
 
+    function mint(uint256 amount, address owner) external {
+        _mint(amount, owner);
+    }
+
     function _mint(uint256 amount, address owner) internal {
         totalSupply += amount;
         balanceOf[owner] += amount;
